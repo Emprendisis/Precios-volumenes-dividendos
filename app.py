4d7a5d05c58404bcf543dd8b78a627194837f44e
Emprendisis <emprendisis@gmail.com>
2025-06-12 19:18:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -63,8 +63,10 @@ if st.button("🔽 Obtener datos"):
                 # Agregar dividendos al Excel
                 precios_volumen.index = precios_volumen.index.tz_localize(None)
                 dividends.index = dividends.index.tz_localize(None)
+                precios_volumen = precios_volumen.reset_index()
+                precios_volumen['Date'] = precios_volumen['Date'].dt.strftime('%d/%m/%Y')
                 with pd.ExcelWriter(f"{ticker_input}_datos_completos.xlsx") as writer:
-                    precios_volumen.to_excel(writer, sheet_name="Precios y Volumen")
+                    precios_volumen.to_excel(writer, sheet_name="Precios y Volumen", index=False)
                     dividends.rename("Dividendo Pagado").to_frame().to_excel(writer, sheet_name="Dividendos")
                 with open(f"{ticker_input}_datos_completos.xlsx", "rb") as f:
                     st.download_button(
