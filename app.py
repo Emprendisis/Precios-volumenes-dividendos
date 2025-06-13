1bc454402fa2dd4bc8330ba1448e2952a1d00244
Emprendisis <emprendisis@gmail.com>
2025-06-12 20:51:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,9 @@ if st.button("🔽 Obtener datos"):
                 precios_volumen['Date'] = precios_volumen['Date'].dt.strftime('%d/%m/%Y')
                 with pd.ExcelWriter(f"{ticker_input}_datos_completos.xlsx") as writer:
                     precios_volumen.to_excel(writer, sheet_name="Precios y Volumen", index=False)
-                    dividends.rename("Dividendo Pagado").to_frame().to_excel(writer, sheet_name="Dividendos")
+                    dividends_df.to_excel(writer, sheet_name="Dividendos", index=False)
+                    dividends_df = dividends.rename("Dividendo Pagado").to_frame().reset_index()
+                    dividends_df['Date'] = dividends_df['Date'].dt.strftime('%d/%m/%Y')                    
                 with open(f"{ticker_input}_datos_completos.xlsx", "rb") as f:
                     st.download_button(
                         "📥 Descargar Excel completo (con dividendos)",
